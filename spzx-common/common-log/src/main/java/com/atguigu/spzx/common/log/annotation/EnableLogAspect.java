2d2db599a80ff64587a4e39d6644d074135f2069
JF <Jianfeng_zheng@yeah.net>
2025-07-27 20:23:45 +0800

    第四次修改 23

--- spzx-common/common-log/src/main/java/com/atguigu/spzx/common/log/annotation/EnableLogAspect.java
+++ spzx-common/common-log/src/main/java/com/atguigu/spzx/common/log/annotation/EnableLogAspect.java
@@ -11,6 +11,7 @@ import java.lang.annotation.Target;
 // git修改测试
 // 第二次修改
 // 第三次修改
+// 第四次修改
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Import(value = LogAspect.class)            // 通过Import注解导入日志切面类到Spring容器中
